9c4b265e6c940875e02d2c360a30a978b94c2f6b
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2025-04-19 19:45:52 +0100

    feat: remove unused files

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  // return a list of URLs to prerender at build time
+  async prerender() {
+    return ["/"];
+  },
 } satisfies Config;
